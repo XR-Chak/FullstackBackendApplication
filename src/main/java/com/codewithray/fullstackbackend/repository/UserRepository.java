bf7908ac4b37b46f3a134e1350ce4fa866c4bb29
raychak <raychak@asl.com.hk>
2023-04-26 10:03:19 +0800

    My first trial

---
+++ src/main/java/com/codewithray/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codewithray.fullstackbackend.repository;
+
+import com.codewithray.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
