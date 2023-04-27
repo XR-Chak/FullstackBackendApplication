4895bdcd3154d34cbe705500d02ba62fabe8004d
RayChak <raychak@asl.com.hk>
2023-04-28 00:33:32 +0800

    asd

--- src/main/java/com/codewithray/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithray/fullstackbackend/controller/UserController.java
@@ -4,13 +4,10 @@ package com.codewithray.fullstackbackend.controller;
 import com.codewithray.fullstackbackend.model.User;
 import com.codewithray.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class UserController {
 
